9f54450b8bec7874a1edda76fc2c54fee5ddcb18
Ritvik Malhotra <ritvikmalhotra0109@gmail.com>
2023-07-30 03:41:17 -0400

    adds sql queries for listings setters

--- src/Listing.java
+++ src/Listing.java
@@ -51,6 +51,15 @@ public class Listing {
 
     public void setPropertyType(String propertyType) {
         this.propertyType = propertyType;
+
+        String sql = "UPDATE Listings SET property_type = ? WHERE list_id = ?";
+        String success = SQL.executeUpdate(sql, this.propertyType, this.listId);
+        System.out.println();
+        if (success.isEmpty()) {
+            System.out.println("Successfully updated property propertyTypen");
+        } else {
+            System.out.println("Error: Failed to update property type in database.");
+        }
     }
 
     public String getTitle() {
@@ -59,6 +68,15 @@ public class Listing {
 
     public void setTitle(String title) {
         this.title = title;
+
+        String sql = "UPDATE Listings SET title = ? WHERE list_id = ?";
+        String success = SQL.executeUpdate(sql, this.title, this.listId);
+        System.out.println();
+        if (success.isEmpty()) {
+            System.out.println("Successfully updated title!\n");
+        } else {
+            System.out.println("Error: Failed to update title type in database.");
+        }
     }
 
     public String getDescription() {
@@ -67,6 +85,15 @@ public class Listing {
 
     public void setDescription(String description) {
         this.description = description;
+
+        String sql = "UPDATE Listings SET description = ? WHERE list_id = ?";
+        String success = SQL.executeUpdate(sql, this.description, this.listId);
+        System.out.println();
+        if (success.isEmpty()) {
+            System.out.println("Successfully updated description type!\n");
+        } else {
+            System.out.println("Error: Failed to update description type in database.");
+        }
     }
 
     public double getPricePerNight() {
@@ -75,6 +102,15 @@ public class Listing {
 
     public void setPricePerNight(double pricePerNight) {
         this.pricePerNight = pricePerNight;
+
+        String sql = "UPDATE Listings SET price_per_night = ? WHERE list_id = ?";
+        String success = SQL.executeUpdate(sql, this.pricePerNight, this.listId);
+        System.out.println();
+        if (success.isEmpty()) {
+            System.out.println("Successfully updated price!\n");
+        } else {
+            System.out.println("Error: Failed to update price type in database.");
+        }
     }
 
     public String getAddress() {
@@ -83,6 +119,15 @@ public class Listing {
 
     public void setAddress(String address) {
         this.address = address;
+
+        String sql = "UPDATE Listings SET address = ? WHERE list_id = ?";
+        String success = SQL.executeUpdate(sql, this.address, this.listId);
+        System.out.println();
+        if (success.isEmpty()) {
+            System.out.println("Successfully updated address!\n");
+        } else {
+            System.out.println("Error: Failed to update address type in database.");
+        }
     }
 
     public String getCity() {
@@ -91,6 +136,15 @@ public class Listing {
 
     public void setCity(String city) {
         this.city = city;
+
+        String sql = "UPDATE Listings SET city = ? WHERE list_id = ?";
+        String success = SQL.executeUpdate(sql, this.city, this.listId);
+        System.out.println();
+        if (success.isEmpty()) {
+            System.out.println("Successfully updated city!\n");
+        } else {
+            System.out.println("Error: Failed to update city type in database.");
+        }
     }
 
     public String getCountry() {
@@ -99,6 +153,15 @@ public class Listing {
 
     public void setCountry(String country) {
         this.country = country;
+
+        String sql = "UPDATE Listings SET country = ? WHERE list_id = ?";
+        String success = SQL.executeUpdate(sql, this.country, this.listId);
+        System.out.println();
+        if (success.isEmpty()) {
+            System.out.println("Successfully updated country!\n");
+        } else {
+            System.out.println("Error: Failed to update country type in database.");
+        }
     }
 
     public int getPostalCode() {
@@ -107,6 +170,15 @@ public class Listing {
 
     public void setPostalCode(int postalCode) {
         this.postalCode = postalCode;
+
+        String sql = "UPDATE Listings SET postal_code = ? WHERE list_id = ?";
+        String success = SQL.executeUpdate(sql, this.postalCode, this.listId);
+        System.out.println();
+        if (success.isEmpty()) {
+            System.out.println("Successfully updated postal code!\n");
+        } else {
+            System.out.println("Error: Failed to update postal code type in database.");
+        }
     }
 
     public String getUnitRoomNumber() {
@@ -115,6 +187,15 @@ public class Listing {
 
     public void setUnitRoomNumber(String unitRoomNumber) {
         this.unitRoomNumber = unitRoomNumber;
+
+        String sql = "UPDATE Listings SET unit_room_number = ? WHERE list_id = ?";
+        String success = SQL.executeUpdate(sql, this.unitRoomNumber, this.listId);
+        System.out.println();
+        if (success.isEmpty()) {
+            System.out.println("Successfully updated unit/room number!\n");
+        } else {
+            System.out.println("Error: Failed to update unit/room number type in database.");
+        }
     }
 
     public double getLongitude() {
@@ -123,6 +204,15 @@ public class Listing {
 
     public void setLongitude(double longitude) {
         this.longitude = longitude;
+
+        String sql = "UPDATE Listings SET longitude = ? WHERE list_id = ?";
+        String success = SQL.executeUpdate(sql, this.longitude, this.listId);
+        System.out.println();
+        if (success.isEmpty()) {
+            System.out.println("Successfully updated longitude!\n");
+        } else {
+            System.out.println("Error: Failed to update longitude type in database.");
+        }
     }
 
     public double getLatitude() {
@@ -131,6 +221,15 @@ public class Listing {
 
     public void setLatitude(double latitude) {
         this.latitude = latitude;
+
+        String sql = "UPDATE Listings SET latitude = ? WHERE list_id = ?";
+        String success = SQL.executeUpdate(sql, this.latitude, this.listId);
+        System.out.println();
+        if (success.isEmpty()) {
+            System.out.println("Successfully updated latitude!\n");
+        } else {
+            System.out.println("Error: Failed to update latitude type in database.");
+        }
     }
 
     public int getUserId() {
